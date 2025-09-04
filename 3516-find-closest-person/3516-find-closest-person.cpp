72cad01635f7416be35738a141c98fa5f6cfe8c4
Rahul Singh <rahulsingh28062005@gmail.com>
2025-09-04 23:16:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (21.23%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a=abs(x-z);
+        int b=abs(y-z);
+        if(a<b)
+        return 1;
+        if(a>b)
+        return 2;
+        return 0;
+    }
+};
