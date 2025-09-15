17ac10c9b11000b05e958c1245fafa4d36039bae
Rahul Singh <rahulsingh28062005@gmail.com>
2025-09-15 22:19:15 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.81%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        vector<int> freq(26,0);
+        for(int i=0;i<brokenLetters.size();i++)
+        freq[brokenLetters[i]-'a']++;
+        text+=" ";
+        bool flag=true;int c=0;
+        for(int i=0;i<text.size();i++){
+            //cout<<freq[text[i]]<<endl;
+            if(text[i] == ' '){
+                if(flag == true)
+                c++;
+                else
+                flag=true;
+                continue;
+            }
+            char ch=text[i];
+            if(freq[ch - 'a'] != 0)
+            flag=false;
+        }
+        return c;
+    }
+};
