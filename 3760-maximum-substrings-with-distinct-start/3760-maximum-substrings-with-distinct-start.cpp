668eedcc19c30af8d85a860a46419bbd107ae871
Rahul Singh <rahulsingh28062005@gmail.com>
2025-12-29 18:52:36 +0530

    Time: 11 ms (79.68%), Space: 23.8 MB (91.92%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int maxDistinct(string s) {
         vector<int> freq(26,0);
+        int c=0;
         for(int i=0;i<s.size();i++){
             if(freq[s[i]-'a'] == 0)
             c++;
-            freq[s[i] - 'a']++;
+            freq[s[i] - 'a']=1;;
         }
-        int c=0;
-        for(auto it:freq)
-        if(it == 1)
-        c++;
         return c;
     }
 };
