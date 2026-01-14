ef695812d7953b421be3242314ba7c1b55bae2b8
Rahul Singh <rahulsingh28062005@gmail.com>
2026-01-14 21:36:20 +0530

    Time: 0 ms (100%), Space: 7.1 MB (100%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isThree(int n) {
+    int c=0;
+    for(int i=1;i<=n;i++){
+    if(n%i==0)
+    c++;
+    if(c>3)return 0;}
+    if(c==3)
+    return 1;
+    return 0;
+    }
+};
