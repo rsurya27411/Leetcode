c316e57e25120f9f6281e9a06ee3b5ebe1889732
Rahul Singh <rahulsingh28062005@gmail.com>
2026-02-08 23:19:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (84.72%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        vector<int> freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            if(freq[s[i]-'a'] == 2)
+            return s[i];
+        } 
+        return ' ';
+    }
+};
