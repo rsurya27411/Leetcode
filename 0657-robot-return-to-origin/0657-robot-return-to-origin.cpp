d5db4325b66a56659ab8c34b8c7dfc0dbfa3faf8
Rahul Singh <rahulsingh28062005@gmail.com>
2026-04-05 22:50:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+    int c1=0,c2=0;
+for(int i=0;i<moves.size();i++)
+    {
+    if(moves[i]=='R')
+    c1++;
+    if(moves[i]=='L')
+    c1--;
+    if(moves[i]=='U')
+    c2++;
+    if(moves[i]=='D')
+    c2--;   
+}
+if(c1==0&&c2==0)return true;
+    return false;
+    }
+};
