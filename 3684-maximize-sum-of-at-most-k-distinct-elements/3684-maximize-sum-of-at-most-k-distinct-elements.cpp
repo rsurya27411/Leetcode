f25b9531ad9cc6eed8ad11cb12637834238eddbf
Rahul Singh <rahulsingh28062005@gmail.com>
2026-04-09 23:24:42 +0530

    Time: 6 ms (47.91%), Space: 26.6 MB (85.83%) - LeetHub

--- 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.cpp
+++ 3684-maximize-sum-of-at-most-k-distinct-elements/3684-maximize-sum-of-at-most-k-distinct-elements.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> maxKDistinct(vector<int>& nums, int k) {
+        if(nums.size() == 1)return nums;
         sort(nums.begin(),nums.end());
         vector<int> ans;
-        for(int i=nums.size()-1;i>0;i--){
+        int i=0;
+        for(i=nums.size()-1;i>0;i--){
             if(ans.size() == k)break;
-            if(nums[i] != nums[i-1])
+            if(i>=0 && nums[i] != nums[i-1])
             ans.push_back(nums[i]);
         }
-        if(nums[0] == nums[1])
-        ans.push_back(nums[1]);
+        if(ans.size() < k){
+            if(find(ans.begin(),ans.end(),nums[i]) == ans.end())
+            ans.push_back(nums[i]);
+        }
+        cout<<i;
         return ans;
     }
 };
