ccee66219b4313523ff8010ab9df4fefa3065816
Rahul Singh <rahulsingh28062005@gmail.com>
2026-06-05 23:53:21 +0530

    Time: 0 ms (100%), Space: 29.3 MB (78.54%) - LeetHub

--- 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int area = dimensions[0][0] * dimensions[0][1];
-        double diag = sqrt(pow(dimensions[0][0],2) + pow(dimensions[0][1],2));
-        cout<<diag;
-        for(int i=1;i<dimensions.size();i++){
-            double val=sqrt(pow(dimensions[i][0],2) + pow(dimensions[i][1],2));
-            cout<<val;
-            if(val >= diag){
-                area = dimensions[i][0] * dimensions[i][1];
-                diag = val;
-            }
+    int area = dimensions[0][0] * dimensions[0][1];
+    double diag = sqrt(1LL * dimensions[0][0] * dimensions[0][0] +
+                       1LL * dimensions[0][1] * dimensions[0][1]);
+
+    for (int i = 1; i < dimensions.size(); i++) {
+        int curArea = dimensions[i][0] * dimensions[i][1];
+        double curDiag = sqrt(1LL * dimensions[i][0] * dimensions[i][0] +
+                              1LL * dimensions[i][1] * dimensions[i][1]);
+
+        if (curDiag > diag || (curDiag == diag && curArea > area)) {
+            diag = curDiag;
+            area = curArea;
         }
-        return area;
     }
+    return area;
+}
 };
